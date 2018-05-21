de2efc2bd04709bc963561f37f83a0a903d3adab
Jacobfh <jfh708@gmail.com>
2018-05-21 07:38:13 -0400

    show page linking with id, not picking up product info

--- faust-furniture-app/config/routes.rb
+++ faust-furniture-app/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'inventory/index'
-  get 'inventory/:id' => 'inventory#description'
+  get 'inventory/description/:id' => 'inventory#description'
   get 'welcome/index'
   root 'welcome#index'
 end
